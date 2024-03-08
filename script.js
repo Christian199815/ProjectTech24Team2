7cc8722beaeb49773c7bdd9c9cfd4082033664eb
David Bijl <davidbijl321@gmail.com>
2024-03-08 10:08:25 +0100

    dingetjes gemaakt

---
+++ script.js
@@ -0,0 +1,13 @@
+
+document.getElementById("searchInput").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      searchFunction();
+    }
+  });
+  
+  function searchFunction() {
+    var searchQuery = document.getElementById("searchInput").value;
+    console.log("Search query: " + searchQuery);
+  }
+  
