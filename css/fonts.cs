02fc5e51e1fed2d0e4b51bc23c990e7cbb519b71
kim <Kimvz19@users.noreply.github.com>
2024-03-08 10:51:05 +0100

    the general opstelling

---
+++ css/fonts.cs
@@ -0,0 +1,131 @@
+/* BLACK */
+
+@font-face {
+    font-family: Prompt;
+    src: url("../fonts/Prompt-Black.ttf") format("truetype");
+    font-weight: 900;
+}
+
+@font-face {
+    font-family: Prompt;
+    src: url("../fonts/Prompt-BlackItalic.ttf") format("truetype");
+    font-weight: 900;
+}
+
+
+/* EXTRA BOLD */
+
+@font-face {
+    font-family: Prompt;
+    src: url("../fonts/Prompt-ExtraBold.ttf") format("truetype");
+    font-weight: 800;
+}
+
+@font-face {
+    font-family: Prompt;
+    src: url("../fonts/Prompt-ExtraBoldItalic.ttf") format("truetype");
+    font-weight: 800;
+}
+
+
+/* BOLD */
+
+@font-face {
+    font-family: Prompt;
+    src: url("../fonts/Prompt-Bold.ttf") format("truetype");
+    font-weight: 700;
+}
+
+@font-face {
+    font-family: Prompt;
+    src: url("../fonts/Prompt-BoldItalic.ttf") format("truetype");
+    font-weight: 700;
+}
+
+/* SEMI BOLD */
+
+@font-face {
+    font-family: Prompt;
+    src: url("../fonts/Prompt-SemiBold.ttf") format("truetype");
+    font-weight: 600;
+}
+
+@font-face {
+    font-family: Prompt;
+    src: url("../fonts/Prompt-SemiBoldItalic.ttf") format("truetype");
+    font-weight: 600;
+}
+
+
+/* MEDIUM */
+
+@font-face {
+    font-family: Prompt;
+    src: url("../fonts/Prompt-Medium.ttf") format("truetype");
+    font-weight: 500;
+}
+
+@font-face {
+    font-family: Prompt;
+    src: url("../fonts/Prompt-MediumItalic.ttf") format("truetype");
+    font-weight: 500;
+}
+
+
+/* REGULAR */
+
+@font-face {
+    font-family: Prompt;
+    src: url("../fonts/Prompt-Regular.ttf") format("truetype");
+    font-weight: 400;
+}
+
+@font-face {
+    font-family: Prompt;
+    src: url("../fonts/Prompt-RegularItalic.ttf") format("truetype");
+    font-weight: 400;
+}
+
+/* LIGHT */
+
+@font-face {
+    font-family: Prompt;
+    src: url("../fonts/Prompt-Light.ttf") format("truetype");
+    font-weight: 300;
+}
+
+@font-face {
+    font-family: Prompt;
+    src: url("../fonts/Prompt-LightItalic.ttf") format("truetype");
+    font-weight: 300;
+}
+
+
+/* EXTRA LIGHT */
+
+@font-face {
+    font-family: Prompt;
+    src: url("../fonts/Prompt-ExtraLight.ttf") format("truetype");
+    font-weight: 200;
+}
+
+@font-face {
+    font-family: Prompt;
+    src: url("../fonts/Prompt-ExtraLightItalic.ttf") format("truetype");
+    font-weight: 200;
+}
+
+
+/* THIN */
+
+@font-face {
+    font-family: Prompt;
+    src: url("../fonts/Prompt-Thin.ttf") format("truetype");
+    font-weight: 100;
+}
+
+@font-face {
+    font-family: Prompt;
+    src: url("../fonts/Prompt-ThinItalic.ttf") format("truetype");
+    font-weight: 100;
+}
