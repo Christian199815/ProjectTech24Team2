7bd64f6f8f4a32c3bca526557f65cbdfdd76ac73
Christian Donker <45289692+Christian199815@users.noreply.github.com>
2024-04-05 13:08:08 +0200

    Error

--- routes/errorRoute.js
+++ routes/errorRoute.js
@@ -4,9 +4,8 @@ const router = express.Router();
 
 
 router.get('/error', async (req, res, next) => {
-    res.status(500).render('error', { error: "Plopkoek Tom" });
-
-
+    const user = req.session.user;
+    res.status(500).render('pages/error', { error: "Plopkoek Tom" , user});
 });
 
 module.exports = router;
